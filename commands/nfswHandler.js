646a65c1ce850031285fe8a4973fa51c55d16cb7
lexus-SGR <whittakertech0@gmail.com>
2025-05-31 16:24:46 +0300

    Update nfswHandler.js

--- commands/nfswHandler.js
+++ commands/nfswHandler.js
@@ -1,11 +1,14 @@
+// nsfwHandler.js
 const { warnUser } = require('./warnUser');
 
-if (nsfwScore > 0.7) {
-  await sock.sendMessage(from, {
-    text: "🚫 NSFW content detected! Message removed."
-  }, { quoted: msg });
+module.exports.handleNsfwCheck = async function handleNsfwCheck(sock, from, msg, nsfwScore) {
+  if (nsfwScore > 0.7) {
+    await sock.sendMessage(from, {
+      text: "🚫 NSFW content detected! Message removed."
+    }, { quoted: msg });
 
-  await sock.deleteMessage(from, { id: msg.key.id, remoteJid: from, fromMe: false });
+    await sock.deleteMessage(from, { id: msg.key.id, remoteJid: from, fromMe: false });
 
-  await warnUser(sock, from, msg.key.participant || msg.key.remoteJid, msg);
-}
+    await warnUser(sock, from, msg.key.participant || msg.key.remoteJid, msg);
+  }
+};

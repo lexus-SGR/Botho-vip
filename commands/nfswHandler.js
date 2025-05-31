083eb8df66849eec923a0d1f5f38093579858946
lexus-SGR <whittakertech0@gmail.com>
2025-05-31 08:49:21 +0300

    Create nfswHandler.js

---
+++ commands/nfswHandler.js
@@ -0,0 +1,11 @@
+const { warnUser } = require('./warnUser');
+
+if (nsfwScore > 0.7) {
+  await sock.sendMessage(from, {
+    text: "🚫 NSFW content detected! Message removed."
+  }, { quoted: msg });
+
+  await sock.deleteMessage(from, { id: msg.key.id, remoteJid: from, fromMe: false });
+
+  await warnUser(sock, from, msg.key.participant || msg.key.remoteJid, msg);
+}

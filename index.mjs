175700f45b6034694528f1145f2b6c9cbf50c3c1
lexus-SGR <whittakertech0@gmail.com>
2025-06-01 13:31:58 +0300

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -180,7 +180,7 @@ async function startBot() {
   sock.ev.on("messages.upsert", async ({ messages }) => {
     try {
       const msg = messages[0];
-      if (!msg.message || msg.key.fromMe) return;
+      //if (!msg.message || msg.key.fromMe) return;
 
       const from = msg.key.remoteJid;
       const isGroup = from.endsWith("@g.us");

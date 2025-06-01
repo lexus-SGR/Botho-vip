8461f193e3fbcfea4f8c9d05f3673b64cec47473
lexus-SGR <whittakertech0@gmail.com>
2025-06-01 07:53:42 +0300

    Delete commands directory

--- commands/nsfwwarn.js
+++
@@ -1,7 +0,0 @@
-// warnUser.js
-module.exports.warnUser = async function warnUser(sock, from, userId, msg) {
-  await sock.sendMessage(from, {
-    text: `⚠️ Warning to user: @${userId.split('@')[0]} for sending NSFW content!`,
-    mentions: [userId]
-  }, { quoted: msg });
-};
